244ab95938075f41eaf37b0ee16539c1207e1eb6
moel-yag <moel-yag@student.1337.ma>
2025-02-23 10:30:33 +0000

    Update server.c

---
+++ server.c
@@ -0,0 +1,34 @@
+#include "utils.h"
+
+static void	handle_signal(int sig, siginfo_t *info, void *context)
+{
+	static int	bit_count = 0;
+	static char	c = 0;
+
+	(void)context;
+	if (sig == SIGUSR1)
+		c |= (1 << bit_count);
+	bit_count++;
+	if (bit_count == 8)
+	{
+		write(1, &c, 1);
+		bit_count = 0;
+		c = 0;
+	}
+	kill(info->si_pid, SIGUSR1); // Acknowledge receipt
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	ft_printf("Server PID: %d\n", getpid());
+	sa.sa_sigaction = handle_signal;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
+}
