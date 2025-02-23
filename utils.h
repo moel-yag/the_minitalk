6c6aafe874e2b0dc491366fa23bff5c83a86c47e
moel-yag <moel-yag@student.1337.ma>
2025-02-23 12:18:54 +0000

    going on..

---
+++ utils.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moel-yag <moel-yag@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/23 12:15:39 by moel-yag          #+#    #+#             */
+/*   Updated: 2025/02/23 12:17:44 by moel-yag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+
+
+void    send_bit(int pid, char bit);
+void    send_char(int pid, char c);
+void    handle_signal(int sig, siginfo_t *info, void *context);
+
+#endif
